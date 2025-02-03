0db8b8e3b81aeffb6404c595a6d93cd8f5fec195
Lys Paul Lukuka <lyslukuka@gmail.com>
2025-02-03 11:32:37 -0600

    added crossorgin

--- src/main/java/com/lyspaul/emailWriter/app/EmailGeneratorController.java
+++ src/main/java/com/lyspaul/emailWriter/app/EmailGeneratorController.java
@@ -3,11 +3,9 @@ package com.lyspaul.emailWriter.app;
 
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin("http://localhost:5173/")
 @RestController
 @RequestMapping("/api/email")
 @AllArgsConstructor
